4d9ffa3c9b335be01ea2d8ae3db76dcb207b72d3
EC2 Default User <ec2-user@ip-172-31-3-81.ec2.internal>
2020-09-19 09:44:30 +0000

    [Finish] 4-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  # get 'users/show'
   root 'post_images#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :post_images, only: [:new, :create, :index, :show] do
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
 #     今回、いいねの詳細ページは作成しません。
 # favoritesのshowページが不要で、idの受け渡しも必要ないので、resourceとなっています。
     resources :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show]
 end
