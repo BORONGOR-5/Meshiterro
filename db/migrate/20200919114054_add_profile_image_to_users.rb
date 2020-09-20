b1ba18d55c9d0b577fef6d77b6b995d0d5f0fb57
EC2 Default User <ec2-user@ip-172-31-3-81.ec2.internal>
2020-09-20 05:03:10 +0000

    [Finish] 4-6

--- db/migrate/20200919114054_add_profile_image_to_users.rb
+++ db/migrate/20200919114054_add_profile_image_to_users.rb
@@ -1,5 +1,7 @@
 class AddProfileImageToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :profile_imaage_id, :string
+    add_column :users, :profile_image_id, :string
+
+    # rename_column :users, :profile_imaage_id, :profile_image_id
   end
 end
--- db/migrate/20200919135852_rename_profile_imaage_id_column_to_users.rb
+++
@@ -1,4 +0,0 @@
-class RenameProfileImaageIdColumnToUsers < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
